3e79a21dff264104cbbd503d903e8eead1f064ff
Sukhbinder <Sukhbinder@yahoo.comom>
2022-03-09 08:48:51 +0530

    updated make_video.py

--- make_video.py
+++ make_video.py
@@ -5,7 +5,7 @@ import datetime
 
 
 
-def make_videos():
+def make_videos(hour=None):
     now = datetime.datetime.now()
     files=  os.listdir()
     files.sort(key =os.path.getmtime)
@@ -17,31 +17,37 @@ def make_videos():
     writer = iio.get_writer(r'/home/pi/Desktop/images/v_{}.mp4'.format(nowstr, fps=2))
     for im in hours:
         try:
-           writer.append_data(iio.imread(im))
+           im_i = iio.imread(im)
+           writer.append_data(im_i)
         except Exception as ex:
-           with open("error.txt" "w") as fout:
+           with open("error.txt", "w") as fout:
                 fout.write("ERROR with \n{0}\n{1}".format(im, ex))
            continue
     writer.close()
 
-def all_till_now():
-    now = datetime.datetime.now()
+def all_till_now(datestr=None):
     files = os.listdir()
     files.sort(key=os.path.getmtime)
-    datestr = now.strftime("%m_%d_%Y_")
+    if datestr is None:
+        now = datetime.datetime.now()
+        datestr = now.strftime("%m_%d_%Y_")
     files = [ f for f in files if f.endswith(".png")]
     fname = r"/home/pi/Desktop/images/v_{}_overval.mp4".format(datestr)
-    writer = iio.get_writer(fname, fps=5)
+    writer = iio.get_writer(fname, fps=10)
     for im in files:
+        print("processing {}".format(im))
         try:
-            writer.append_data(iio.imread(im))
+            im_i = iio.imread(im)
+            if len(im_i) != 0:
+               writer.append_data(im_i)
         except Exception as ex:
-            print(ex)
+            print(im, ex)
             continue
     writer.close()
 
 if __name__ == "__main__":
     os.chdir(r"/home/pi/Desktop/images")
+    now = datetime.datetime.now()
     make_videos()
     all_till_now()
 
